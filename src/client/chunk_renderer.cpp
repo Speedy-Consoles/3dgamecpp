6e095d0285820d3324fece972c7bef73b32ac25f
Speedy Consoles <rubihome@gmx.de>
2015-03-22 18:02:54 +0100

    fixed texture parameter enum error

--- src/client/chunk_renderer.cpp
+++ src/client/chunk_renderer.cpp
@@ -29,6 +29,13 @@ void ChunkRenderer::loadTextures() {
 	GLsizei layerCount = 256;
 	GLsizei mipLevelCount = 7;
 
+	GLint maxLayerCount;
+	glGetIntegerv(GL_MAX_ARRAY_TEXTURE_LAYERS, &maxLayerCount);
+	if (maxLayerCount < layerCount) {
+		LOG(ERROR, "Not enough levels available for texture");
+		return;
+	}
+
 	SDL_Surface *img = IMG_Load("img/textures_1.png");
 	if (!img) {
 		LOG(ERROR, "Textures could not be loaded");
@@ -84,12 +91,14 @@ void ChunkRenderer::loadTextures() {
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_BASE_LEVEL, 0);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAX_LEVEL, mipLevelCount - 1);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_GENERATE_MIPMAP, GL_TRUE);
+	logOpenGLError();
 
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glGenerateMipmap(GL_TEXTURE_2D_ARRAY);
+	logOpenGLError();
 }
 
 void ChunkRenderer::setConf(const GraphicsConf &conf) {
