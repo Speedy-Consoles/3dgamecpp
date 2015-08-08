d004c8a8ae8d5d8e45fd3e79bc3f2c5859927583
Speedy Consoles <rubihome@gmx.de>
2015-08-08 15:29:03 +0200

    fixed rerender on chunk borders

--- src/client/local_server_interface.cpp
+++ src/client/local_server_interface.cpp
@@ -95,7 +95,34 @@ void LocalServerInterface::setSelectedBlock(uint8 block) {
 void LocalServerInterface::placeBlock(vec3i64 blockCoords, uint8 blockType) {
 	client->getChunkManager()->placeBlock(blockCoords, blockType);
 	// TODO tell world
-	client->getGraphics()->getRenderer()->rerenderChunk(bc2cc(blockCoords));
+	// TODO maybe move this to graphics or something
+	vec3i64 cc = bc2cc(blockCoords);
+	vec3ui8 icc = bc2icc(blockCoords);
+	client->getGraphics()->getRenderer()->rerenderChunk(cc);
+	for (size_t i = 0; i < 27; i++) {
+		if (i == BIG_CUBE_CYCLE_BASE_INDEX)
+			continue;
+		bool rerender = true;
+		for (int d = 0; d < 3 && rerender == true; d++) {
+			switch (BIG_CUBE_CYCLE[i][d]) {
+				case 0:
+					break;
+				case -1:
+					if (icc[d] != 0)
+						rerender = false;
+					break;
+				case 1:
+					if (icc[d] != Chunk::WIDTH - 1)
+						rerender = false;
+					break;
+				default:
+					rerender = false;
+					break;
+			}
+		}
+		if (rerender)
+			client->getGraphics()->getRenderer()->rerenderChunk(cc + BIG_CUBE_CYCLE[i].cast<int64>());
+	}
 }
 
 void LocalServerInterface::toggleFly() {
