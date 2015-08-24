d6c627d709d67cf8400057108c03c3ebb5b3aaec
Lars Drögemüller <larstd90@gmail.com>
2015-08-24 14:35:43 +0200

    Refactored Perlin a little

--- src/shared/game/perlin.cpp
+++ src/shared/game/perlin.cpp
@@ -19,79 +19,91 @@ Perlin::Perlin(uint64 seed) {
     }
 }
 
-double Perlin::octavePerlin(double x, double y, double z, int octaves,
-        double persistence) {
+double Perlin::octavePerlin(double x, double y, double z, int octaves, double persistence) {
     double total = 0;
     double frequency = 1;
     double amplitude = 1;
-    double maxValue = 0; // Used for normalizing result to 0.0 - 1.0
+    double max_value = 0;
     for (int i = 0; i < octaves; i++) {
-        total += perlin(x * frequency, y * frequency, z * frequency)
-                * amplitude;
-        maxValue += amplitude;
+        total += perlin(x * frequency, y * frequency, z * frequency) * amplitude;
+        max_value += amplitude;
         amplitude *= persistence;
         frequency *= 2;
     }
-    return total / maxValue;
+    return total / max_value;
 }
 
 double Perlin::perlin(double x, double y, double z) {
-    int xi = (int) floor(x) & 255; // Calculate the "unit cube" that the point asked will be located in
-    int yi = (int) floor(y) & 255; // The left bound is ( |_x_|,|_y_|,|_z_| ) and the right bound is that
-    int zi = (int) floor(z) & 255; // plus 1. Next we calculate the location (from 0.0 to 1.0) in that cube.
-    double xf = x - floor(x); // We also fade the location to smooth the result.
-    double yf = y - floor(y);
-    double zf = z - floor(z);
-    double u = fade(xf);
-    double v = fade(yf);
-    double w = fade(zf);
-    int aaa, aba, aab, abb, baa, bba, bab, bbb;
-    aaa = p[p[p[xi] + yi] + zi];
-    aba = p[p[p[xi] + yi + 1] + zi];
-    aab = p[p[p[xi] + yi] + zi + 1];
-    abb = p[p[p[xi] + yi + 1] + zi + 1];
-    baa = p[p[p[xi + 1] + yi] + zi];
-    bba = p[p[p[xi + 1] + yi + 1] + zi];
-    bab = p[p[p[xi + 1] + yi] + zi + 1];
-    bbb = p[p[p[xi + 1] + yi + 1] + zi + 1];
+	// lowest corner of the cell, opposite corner have xi + 1 etc
+    const int xi = (int) floor(x) & 255;
+    const int yi = (int) floor(y) & 255;
+    const int zi = (int) floor(z) & 255;
+
+	// relative position in the cell
+    const double xf = x - floor(x);
+    const double yf = y - floor(y);
+    const double zf = z - floor(z);
+
+	// fade constants to smooth the solution
+    const double u = fade(xf);
+    const double v = fade(yf);
+    const double w = fade(zf);
+
+	// calculate pseudorandom hashes for all the corners
+	const int aaa = p[p[p[xi] + yi] + zi];
+	const int aba = p[p[p[xi] + yi + 1] + zi];
+	const int aab = p[p[p[xi] + yi] + zi + 1];
+	const int abb = p[p[p[xi] + yi + 1] + zi + 1];
+	const int baa = p[p[p[xi + 1] + yi] + zi];
+	const int bba = p[p[p[xi + 1] + yi + 1] + zi];
+	const int bab = p[p[p[xi + 1] + yi] + zi + 1];
+	const int bbb = p[p[p[xi + 1] + yi + 1] + zi + 1];
+
+	// multiply the relative coordinate in the cell with the random gradient and lerp it together
     double x1, x2, y1, y2;
-    x1 = lerp(grad(aaa, xf, yf, zf), // The gradient function calculates the dot product between a pseudorandom
-            grad(baa, xf - 1, yf, zf), // gradient vector and the vector from the input coordinate to the 8
-            u); // surrounding points in its unit cube.
-    x2 = lerp(grad(aba, xf, yf - 1, zf), // This is all then lerped together as a sort of weighted average based on the faded (u,v,w)
-            grad(bba, xf - 1, yf - 1, zf), // values we made earlier.
-            u);
+    x1 = lerp(grad(aaa, xf, yf, zf), grad(baa, xf - 1, yf, zf), u);
+    x2 = lerp(grad(aba, xf, yf - 1, zf), grad(bba, xf - 1, yf - 1, zf), u);
     y1 = lerp(x1, x2, v);
 
     x1 = lerp(grad(aab, xf, yf, zf - 1), grad(bab, xf - 1, yf, zf - 1), u);
-    x2 = lerp(grad(abb, xf, yf - 1, zf - 1),
-            grad(bbb, xf - 1, yf - 1, zf - 1), u);
+    x2 = lerp(grad(abb, xf, yf - 1, zf - 1), grad(bbb, xf - 1, yf - 1, zf - 1), u);
     y2 = lerp(x1, x2, v);
-    return (lerp(y1, y2, w) + 1) / 2; // For convenience we bound it to 0 - 1 (theoretical min/max before is -1 - 1)
+
+	// rescale the solution to fit into [0, 1]
+    return (lerp(y1, y2, w) + 1) / 2;
 }
 
 double Perlin::grad(int hash, double x, double y, double z) {
-    int h = hash & 15; // Take the hashed value and take the first 4 bits of it (15 == 0b1111)
-    double u = h < 8 /* 0b1000 */? x : y; // If the most significant bit (MSB) of the hash is 0 then set u = x. Otherwise y.
-    double v; // In Ken Perlin's original implementation this was another conditional operator (?:). I
-    // expanded it for readability.
-    if (h < 4 /* 0b0100 */) // If the first and second significant bits are 0 set v = y
-        v = y;
-    else if (h == 12 /* 0b1100 */|| h == 14 /* 0b1110*/)// If the first and second significant bits are 1 set v = x
-        v = x;
-    else
-        // If the first and second significant bits are not equal (0/1, 1/0) set v = z
-        v = z;
-    return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v); // Use the last 2 bits to decide if u and v are positive or negative. Then return their addition.
+	switch (hash & 0xF) {
+		// 0x0 through 0xB correspond to the edges of a cube
+		case 0x0: return + x + y;
+		case 0x1: return - x + y;
+		case 0x2: return + x - y;
+		case 0x3: return - x - y;
+		case 0x4: return + x + z;
+		case 0x5: return - x + z;
+		case 0x6: return + x - z;
+		case 0x7: return - x - z;
+		case 0x8: return + y + z;
+		case 0x9: return - y + z;
+		case 0xA: return + y - z;
+		case 0xB: return - y - z;
+		// these duplicates give some speed in exchange for uniformity
+		case 0xC: return + y + x;
+		case 0xD: return - y + z;
+		case 0xE: return + y - x;
+		case 0xF: return - y - z;
+		// can never happen
+		default: return 0.0;
+	}
 }
 
 double Perlin::fade(double t) {
-    // Fade function as defined by Ken Perlin. This eases coordinate values
-    // so that they will "ease" towards integral values. This ends up smoothing
-    // the final output.
-    return t * t * t * (t * (t * 6 - 15) + 10); // 6t^5 - 15t^4 + 10t^3
+	// 6t^5 - 15t^4 + 10t^3
+    return t * t * t * (t * (t * 6 - 15) + 10);
 }
 
 double Perlin::lerp(double a, double b, double x) {
+	// linear interpolation
     return a + x * (b - a);
 }
